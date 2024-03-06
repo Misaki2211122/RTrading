77a7e74d74de330d249aac6249672a7a02f66957
Daniil <daniil46y@gmail.com>
2024-03-06 16:04:36 +0500

    feat: add InfrastructureNotifications

--- RTrading.Infrastructure.Notifications/ServiceCollection.cs
+++ RTrading.Infrastructure.Notifications/ServiceCollection.cs
@@ -1,6 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace RTrading.Infrastructure.Notifications;
 
-public class ServiceCollection
+public static class ServiceCollection
 {
-    
+    public static void AddInfrastructureNotifications(this IServiceCollection services)
+    {
+    }
 }
